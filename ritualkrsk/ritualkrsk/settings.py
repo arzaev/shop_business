eea3de06eab997cf549987d75fc53c651fb6a90a
Ilya <arzaevilya@gmail.com>
2021-07-16 07:27:28 +0700

    update settings 1

--- ritualkrsk/ritualkrsk/settings.py
+++ ritualkrsk/ritualkrsk/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = config.STATIC_URL
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -126,7 +126,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = config.STATIC_ROOT
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
